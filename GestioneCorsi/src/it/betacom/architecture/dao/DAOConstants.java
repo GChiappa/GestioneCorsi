a45e657a7f90559eec863e16a9517dd6e6c70518
Betacom <Betacom@DESKTOP-30EVD2S.homenet.telecomitalia.it>
2022-06-29 11:49:48 +0200

    DAOConstants aggiornato -GC

--- GestioneCorsi/src/it/betacom/architecture/dao/DAOConstants.java
+++ GestioneCorsi/src/it/betacom/architecture/dao/DAOConstants.java
@@ -3,14 +3,21 @@ package it.betacom.architecture.dao;
 public interface DAOConstants {
 
 	String SELECT_CORSO = "select * from CORSO";
+	String DELETE_CORSO = "delete from CORSO where cod_corso = ?";
+	String UPDATE_CORSO = "update CORSO set cod_docente = ?, nome = ?, data_inizio = ?, data_fine = ?, aula = ? where cod_corso = ?";
 
 	String SELECT_CORSISTA = "select * from CORSISTA";
+	String DELETE_CORSISTA = "delete from CORSISTA where cod_corsista = ?";
+	String UPDATE_CORSISTA = "update CORSISTA set nome = ?, cognome = ?, precedenti_formativi = ? where cod_corsista = ?";
 
 	String SELECT_DOCENTE = "select * from DOCENTE";
+	String FBYID_DOCENTE = "select * from DOCENTE where cod_docente = ?";
 
 	String SELECT_CORSO_CORSISTA = "select * from CORSO_CORSISTA";
+	String DELETE_CORSO_CORSISTA = "delete from CORSO_CORSISTA where cod_corso_corsista = ?";
 
 	String SELECT_COMMENTO = "select * from COMMENTO";
+	String SELECT_COMMENTO_CORSO = "select * from COMMENTO where cod_corso = ?";
 
 	String SELECT_REPORT_ISCRIZIONI = "select * from REPORT_ISCRIZIONI";
 	
