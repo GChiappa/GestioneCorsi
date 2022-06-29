d5174c25f3b6494e1b6b601c49bee93f0f12a5d2
Betacom <Betacom@192.168.1.10>
2022-06-29 17:57:31 +0200

    inizio scrittura test login-lb

---
+++ GestioneCorsi/src/test/it/betacom/businesscomponent/utility/loginUtilityTest.java
@@ -0,0 +1,18 @@
+package test.it.betacom.businesscomponent.utility;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import it.betacom.businesscomponent.utility.loginUtility;
+import it.betacom.security.AlgoritmoMD5;
+
+class loginUtilityTest {
+
+	@Test
+	void test() {
+		String password=null;
+		//password=AlgoritmoMD5.convertiMD5(loginUtility.getadminpass('codeAdmin1',));
+	}
+
+}
