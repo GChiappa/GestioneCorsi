2248023a05094af24ae54d36abe763e8318636a9
Betacom <Betacom@DESKTOP-30EVD2S.homenet.telecomitalia.it>
2022-06-29 12:44:23 +0200

    Oracle Driver e CorsistaDAO-GC

--- GestioneCorsi/src/it/betacom/architecture/dao/CorsistaDAO.java
+++ GestioneCorsi/src/it/betacom/architecture/dao/CorsistaDAO.java
@@ -1,6 +1,7 @@
 package it.betacom.architecture.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.sql.rowset.CachedRowSet;
@@ -12,7 +13,7 @@ public class CorsistaDAO implements DAOConstants, GenericDAO<Corsista> {
 
 	private CachedRowSet rowSet;
 
-	public static CorsistaDAO getFactry() throws SQLException {
+	public static CorsistaDAO getFactory() throws SQLException {
 		return new CorsistaDAO();
 	}
 
@@ -21,33 +22,93 @@ public class CorsistaDAO implements DAOConstants, GenericDAO<Corsista> {
 	}
 
 	@Override
-	public void create(Corsista entity, Connection conn) {
-		// TODO Auto-generated method stub
-		
+	public void create(Corsista entity, Connection conn) throws SQLException {
+
+		rowSet.setCommand(SELECT_CORSISTA);
+		rowSet.execute(conn);
+		rowSet.moveToInsertRow();
+
+		rowSet.updateLong(1, entity.getCodCorsista());
+		rowSet.updateString(2, entity.getNome());
+		rowSet.updateString(3, entity.getCongnome());
+		rowSet.updateString(4, entity.getPrecedentiFormativi());
+
+		rowSet.moveToCurrentRow();
+		rowSet.acceptChanges();
+
 	}
 
 	@Override
-	public void update(Corsista entity, Connection conn) {
-		// TODO Auto-generated method stub
-		
+	public void update(Corsista entity, Connection conn) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement(UPDATE_CORSISTA);
+
+		ps.setString(1, entity.getNome());
+		ps.setString(2, entity.getCongnome());
+		ps.setString(3, entity.getPrecedentiFormativi());
+		ps.setLong(3, entity.getCodCorsista());
+
+		ps.execute();
+		conn.commit();
 	}
 
 	@Override
-	public void delete(Corsista entity, Connection conn) {
-		// TODO Auto-generated method stub
-		
+	public void delete(long id, Connection conn) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement(DELETE_CORSISTA);
+
+		ps.setLong(1, id);
+		ps.execute();
+		conn.commit();
+
 	}
 
 	@Override
-	public Corsista findById(long id, Connection conn) {
-		// TODO Auto-generated method stub
-		return null;
+	public Corsista findById(long id, Connection conn) throws SQLException {
+		Corsista c = null;
+
+		try {
+			rowSet.setCommand(FBYID_CORSISTA);
+			rowSet.setLong(1, id);
+			rowSet.execute(conn);
+
+			rowSet.beforeFirst();
+			rowSet.next();
+
+			c = new Corsista();
+			c.setCodCorsista(rowSet.getLong(1));
+			c.setNome(rowSet.getString(2));
+			c.setCongnome(rowSet.getString(3));
+			c.setPrecedentiFormativi(rowSet.getString(4));
+		} catch (SQLException e) {
+			throw e;
+		}
+		return c;
 	}
 
 	@Override
-	public Corsista[] getAll(Connection conn) {
-		// TODO Auto-generated method stub
-		return null;
+	public Corsista[] getAll(Connection conn) throws SQLException {
+		Corsista[] corsisti = null;
+		Corsista c;
+		try {
+			rowSet.setCommand(FBYID_CORSISTA);
+			rowSet.execute(conn);
+
+			rowSet.last();
+			corsisti = new Corsista[rowSet.getRow()];
+
+			rowSet.beforeFirst();
+			for (int i = 0; rowSet.next(); i++) {
+				c = new Corsista();
+				c.setCodCorsista(rowSet.getLong(1));
+				c.setNome(rowSet.getString(2));
+				c.setCongnome(rowSet.getString(3));
+				c.setPrecedentiFormativi(rowSet.getString(4));
+				corsisti[i] = c;
+			}
+
+		} catch (SQLException e) {
+			throw e;
+		}
+		return corsisti;
 	}
 
 }
---
+++ GestioneCorsi/src/test/it/betacom/architecture/dao/CorsistaDAOTest.java
@@ -0,0 +1,90 @@
+package test.it.betacom.architecture.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import it.betacom.BC.model.Corsista;
+import it.betacom.architecture.dao.CorsistaDAO;
+import it.betacom.architecture.dbaccess.DBAccess;
+
+@TestMethodOrder(OrderAnnotation.class)
+class CorsistaDAOTest {
+	
+	static Connection conn;
+	static Corsista corsista;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		conn = DBAccess.getConnection();
+		corsista = new Corsista();
+		corsista.setCodCorsista(15846);
+		corsista.setNome("Aldo");
+		corsista.setCongnome("Baglio");
+		corsista.setPrecedentiFormativi("SI");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		try {
+			CorsistaDAO.getFactory().delete(corsista.getCodCorsista(), conn);
+		} catch (SQLException e) {
+			fail("Eccezione durante eliminazione: " + e.getMessage() + "\n\n" + e.getErrorCode());
+		} finally {
+			conn.close();
+			corsista = null;
+		}
+	}
+
+	@Test
+	@Order(1)
+	void testCreate() {
+		try {
+			CorsistaDAO.getFactory().create(corsista, conn);
+		} catch (SQLException e) {
+			fail("Eccezione durante la creazione: " + e.getMessage() + "\n\n" + e.getErrorCode());
+		}
+	}
+
+	@Test
+	@Order(2)
+	void testUpdate() {
+		try {
+			corsista.setPrecedentiFormativi("NO");
+			CorsistaDAO.getFactory().update(corsista, conn);
+		} catch (SQLException e) {
+			fail("Eccezione durante update: " + e.getMessage() + "\n\n" + e.getErrorCode());
+		}
+	}
+
+	@Test
+	@Order(3)
+	void testGetById() {
+		try {
+			Corsista c = CorsistaDAO.getFactory().findById(corsista.getCodCorsista(), conn);
+			assertNotNull(c);
+		} catch (SQLException e) {
+			fail("Eccezione durante find by id: " + e.getMessage() + "\n\n" + e.getErrorCode());
+		}
+	}
+
+	@Test
+	@Order(4)
+	void testGetAll() {
+		try {
+			Corsista[] c = CorsistaDAO.getFactory().getAll(conn);
+			assertNotNull(c);
+		} catch (SQLException e) {
+			fail("Eccezione durante get all: " + e.getMessage() + "\n\n" + e.getErrorCode());
+		}
+	}
+
+}
