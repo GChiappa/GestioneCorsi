73cad303c827beb0f6478064fcb0853dfca9b3cb
Betacom <Betacom@DESKTOP-H0VUI97.homenet.telecomitalia.it>
2022-06-29 12:04:49 +0200

    Aggiunto package test -CC

---
+++ GestioneCorsi/src/it/betacom/architecture/dao/test/CorsoCorsistaDAOTest.java
@@ -0,0 +1,24 @@
+package it.betacom.architecture.dao.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CorsoCorsistaDAOTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
