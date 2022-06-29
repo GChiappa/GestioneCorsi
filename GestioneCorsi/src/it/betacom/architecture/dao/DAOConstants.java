3c3e93f9e5ce44f65b665beb67ca7508174c5eae
Betacom <Betacom@DESKTOP-30EVD2S.homenet.telecomitalia.it>
2022-06-29 12:00:43 +0200

    DAOConstants aggiornato con i findbyid di corso e corsista

--- GestioneCorsi/src/it/betacom/architecture/dao/DAOConstants.java
+++ GestioneCorsi/src/it/betacom/architecture/dao/DAOConstants.java
@@ -4,10 +4,12 @@ public interface DAOConstants {
 
 	String SELECT_CORSO = "select * from CORSO";
 	String DELETE_CORSO = "delete from CORSO where cod_corso = ?";
+	String FBYID_CORSO = "select * from CORSO where cod_corso = ?";
 	String UPDATE_CORSO = "update CORSO set cod_docente = ?, nome = ?, data_inizio = ?, data_fine = ?, aula = ? where cod_corso = ?";
 
 	String SELECT_CORSISTA = "select * from CORSISTA";
 	String DELETE_CORSISTA = "delete from CORSISTA where cod_corsista = ?";
+	String FBYID_CORSISTA = "select * from CORSISTA where cod_corso = ?";
 	String UPDATE_CORSISTA = "update CORSISTA set nome = ?, cognome = ?, precedenti_formativi = ? where cod_corsista = ?";
 
 	String SELECT_DOCENTE = "select * from DOCENTE";
@@ -20,9 +22,7 @@ public interface DAOConstants {
 	String SELECT_COMMENTO_CORSO = "select * from COMMENTO where cod_corso = ?";
 
 	String SELECT_REPORT_ISCRIZIONI = "select * from REPORT_ISCRIZIONI";
-	
+
 	String PASSWORD_AMMINISTRATORE = "select password from AMMINISTRATORE where cod_amministratore = ?";
 
-	
-	
 }
