ecb622a61a57e6335bd9ca69cb2ec09bf03ddd2a
Betacom <Betacom@DESKTOP-P189AP1.homenet.telecomitalia.it>
2022-06-29 09:50:22 +0200

    completo delete - GG

--- GestioneCorsi/SQL/deleteScript.sql
+++ GestioneCorsi/SQL/deleteScript.sql
@@ -4,9 +4,10 @@ drop table corso cascade constraints;
 drop table corso_corsista cascade constraints;
 drop table corsista cascade constraints;
 
+drop sequence corso_seq;
 drop sequence corsista_seq;
 drop sequence corso_corsista_seq;
-drop sequence corso_seq;
+
 
 
 
