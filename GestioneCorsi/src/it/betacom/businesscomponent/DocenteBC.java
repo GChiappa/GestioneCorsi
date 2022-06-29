a673a024cc1c56e55350b8ccba59a4f5b3773ab4
Betacom <Betacom@DESKTOP-P189AP1.homenet.telecomitalia.it>
2022-06-29 15:56:25 +0200

    DocenteBC + Test OK! -GG

--- GestioneCorsi/src/it/betacom/businesscomponent/DocenteBC.java
+++ GestioneCorsi/src/it/betacom/businesscomponent/DocenteBC.java
@@ -1,5 +1,23 @@
 package it.betacom.businesscomponent;
 
-public class DocenteBC {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import it.betacom.architecture.dao.DocenteDAO;
+import it.betacom.architecture.dbaccess.DBAccess;
+import it.betacom.businesscomponent.model.Docente;
+
+
+public class DocenteBC {
+	private Connection conn;
+
+	public Docente findByCod(String cod) throws SQLException {
+		return DocenteDAO.getFactory().findByCod(cod, conn);
+	} 
+	
+	public DocenteBC() throws ClassNotFoundException, IOException {
+		conn = DBAccess.getConnection();
+		
+	}
 }
