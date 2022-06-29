d4f8576409f53bce5eb3a3e956d116654ed5fddf
Betacom <Betacom@DESKTOP-30EVD2S.homenet.telecomitalia.it>
2022-06-29 12:21:47 +0200

    CorsoDAO find by id sistemato -GC

--- GestioneCorsi/src/it/betacom/architecture/dao/CorsoDAO.java
+++ GestioneCorsi/src/it/betacom/architecture/dao/CorsoDAO.java
@@ -56,7 +56,7 @@ public class CorsoDAO implements GenericDAO<Corso>, DAOConstants {
 	}
 
 	@Override
-	public void delete( long id, Connection conn) throws SQLException {
+	public void delete(long id, Connection conn) throws SQLException {
 		PreparedStatement ps;
 		ps = conn.prepareStatement(DELETE_CORSO);
 		ps.setLong(1, id);
@@ -66,10 +66,10 @@ public class CorsoDAO implements GenericDAO<Corso>, DAOConstants {
 	}
 
 	@Override
-	public Corso findById(long id, Connection conn) {
+	public Corso findById(long id, Connection conn) throws SQLException {
 		Corso corso = null;
 		PreparedStatement ps;
-		ps = conn.prepareStatement(SELECT_CORSO_BYID);
+		ps = conn.prepareStatement(FBYID_CORSO);
 		ps.setLong(1, id);
 		ResultSet rs = ps.executeQuery();
 		if (rs.next()) {
@@ -79,27 +79,27 @@ public class CorsoDAO implements GenericDAO<Corso>, DAOConstants {
 			corso.setNome(rs.getString(3));
 			corso.setInizioCorso(rs.getDate(4));
 			corso.setFineCorso(rs.getDate(5));
-			return corso;
+
 		}
+		return corso;
 	}
 
 	@Override
 	public Corso[] getAll(Connection conn) throws SQLException {
 		Corso[] corso = null;
-		Statement stmt=conn.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, 
-				 ResultSet.CONCUR_READ_ONLY);
-		ResultSet rs=stmt.executeQuery(SELECT_CORSO);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet rs = stmt.executeQuery(SELECT_CORSO);
 		rs.last();
-		corso=new Corso[rs.getRow()];
+		corso = new Corso[rs.getRow()];
 		rs.beforeFirst();
-		for(int i=0;rs.next();i++) {
-			Corso c= new Corso();
+		for (int i = 0; rs.next(); i++) {
+			Corso c = new Corso();
 			c.setCodCorso(rs.getLong(1));
 			c.setCodDocente(rs.getString(2));
 			c.setNome(rs.getString(3));
 			c.setInizioCorso(new java.util.Date(rs.getDate(4).getTime()));
 			c.setFineCorso(new java.util.Date(rs.getDate(5).getTime()));
-			corso[i]=c;
+			corso[i] = c;
 		}
 		rs.close();
 		return null;
