9cf1c4c68ea69e9f36df2031fa866360adfc7720
Betacom <Betacom@DESKTOP-H0VUI97>
2022-06-30 15:42:02 +0200

    accessonegato + controllo da testare - CC

--- GestioneCorsi/src/it/betacom/controller/ControlloAdmin.java
+++ GestioneCorsi/src/it/betacom/controller/ControlloAdmin.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.betacom.businesscomponent.utility.loginUtility;
+import it.betacom.security.AlgoritmoMD5;
 
  
 @WebServlet("/ControlloAdmin")
@@ -15,8 +19,39 @@ public class ControlloAdmin extends HttpServlet {
 	private static final long serialVersionUID = -4756704292690882364L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String codAdmin= request.getParameter("admin");
+
+		String password= AlgoritmoMD5.convertiMD5(request.getParameter("password") );
+		
+		HttpSession session = request.getSession();
+		String userpass= null;
+		String adminpass = null;
+		if(codAdmin != null && password!=null) {
+			try {
+				
+				loginUtility lu = new loginUtility();
+				adminpass= lu.getadminpass(codAdmin);
+				 
+				 
+				   if (adminpass!= null)   {
+					 if(adminpass.equals(password)) {
+						 session.setAttribute("admin", codAdmin);
+						 response.sendRedirect("paginaprincipale.jsp");
+					 }else {
+						 response.sendRedirect("accessonegato.jsp");
+					 }				 
+				  
+			} else {
+				response.sendRedirect("accessonegato.jsp");
+			}
+				 
+		}catch(Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc.getMessage());
+			
+		}
+		
+	}
 	}
 
 }
