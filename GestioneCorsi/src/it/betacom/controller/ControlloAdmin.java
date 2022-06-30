fcfac9774a80a6ced7e7eb4f76fd794a1e5d6767
Betacom <Betacom@DESKTOP-H0VUI97>
2022-06-30 15:33:15 +0200

    aggiunto package controller  - CC

---
+++ GestioneCorsi/src/it/betacom/controller/ControlloAdmin.java
@@ -0,0 +1,22 @@
+package it.betacom.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+ 
+@WebServlet("/ControlloAdmin")
+public class ControlloAdmin extends HttpServlet {
+ 
+
+	private static final long serialVersionUID = -4756704292690882364L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
