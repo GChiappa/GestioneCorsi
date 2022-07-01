e40cbd1eab98d7e25d937488d6966f415c3f0e48
Betacom <Betacom@DESKTOP-30EVD2S.fritz.box>
2022-07-01 10:26:31 +0200

    Pagina iniziale e pagine derivate dai pulsanti

--- GestioneCorsi/WebContent/paginaPrincipale.jsp
+++ GestioneCorsi/WebContent/paginaPrincipale.jsp
@@ -1,3 +1,4 @@
+
 <%
 if (session.getAttribute("admin") != null) {
 %>
@@ -13,10 +14,37 @@ if (session.getAttribute("admin") != null) {
 </head>
 <body>
 	<div class="nav"><%@include file="nav.jsp"%></div>
+
 	<div class="container">
-		<h1 style="text-align: center">Management Alfacom</h1>
+		<div class="row">
+			<h1 style="text-align: center">Management Alfacom</h1>
+		</div>
+		<div class="row" style="margin-top: 50px">
+			<div class="col-sm-2 col-md-4"></div>
+
+			<div class="col-xs-12 col-sm-8 col-md-4"
+				style="width: 100%; text-align: center;">
+				<a href="#">
+				<button type="button" class="btn btn-default btn-lg btn-block"
+					style="margin-top: 20px; height: 60px;">Inserisci nuovo Corsista</button>
+				</a>
+				<a href="gestioneCorsisti.jsp">	
+				<button type="button" class="btn btn-default btn-block"
+					style="margin-top: -8px; height: 30px;">Gestione Corsisti</button>
+				</a>
+				<a href="statistiche.jsp">		
+				<button type="button" class="btn btn-default btn-lg btn-block"
+					style="margin-top: 20px;">Visualizza le Statistiche</button>
+				</a>
+				<a href="gestioneCorsi.jsp">		
+				<button type="button" class="btn btn-default btn-lg btn-block"
+					style="margin-top: 20px;">Gestisci Corsi</button>
+				</a>
+			</div>
+			<div class="col-sm-2 col-md-4"></div>
+		</div>
 	</div>
-	
+
 	<footer class="footer"><%@include file="footer.html"%></footer>
 
 </body>
--- GestioneCorsi/src/it/betacom/controller/createCorso.java
+++ GestioneCorsi/src/it/betacom/controller/createCorso.java
@@ -1,7 +1,6 @@
 package it.betacom.controller;
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,54 +8,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import it.betacom.businesscomponent.facade.AdminFacade;
 import it.betacom.businesscomponent.model.Corso;
- 
+
 @WebServlet("/createCorso")
 public class createCorso extends HttpServlet {
- 
+
 	private static final long serialVersionUID = -5347182008070757099L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Corso corso = getCorso(request);
-		if (corso!= null) {
-			try {
-				AdminFacade.getInstance(). 
-				
-			}catch(DAOException | ClassNotFoundException exc) {
-				exc.printStackTrace();
-				throw new ServletException( exc.getMessage());
-				
-			}
-			response.sendRedirect("admin/gestisciArticoli,jsp");
-			
-		}
+
+		// TODO sistemare
+
+		response.sendRedirect("paginaPrincipale.jsp");
+
 	}
-	
-	private Corso getCorso (HttpServletRequest request) {
-		
+
+	private Corso getCorso(HttpServletRequest request) {
+
 		Corso corso = null;
 		try {
 			long id = Long.valueOf(request.getParameter("id"));
 			String nomeCorso = request.getParameter("nomeCorso");
 			String codDocente = request.getParameter("codDocente");
-			Date dataInizio = Date.parse( request.getParameter("dataInizio")) ;
-			
-			
+			// Date dataInizio = Date.parse( request.getParameter("dataInizio")) ;
+
 			corso = new Corso();
 			corso.setCodCorso(id);
-			corso.setCodDocente(codDocente);	
-			corso.setFineCorso(dataFine);
-			corso.setInizioCorso(dataInizio);
+			corso.setCodDocente(codDocente);
+			// corso.setFineCorso(dataFine);
+			// corso.setInizioCorso(dataInizio);
 			corso.setNome(nomeCorso);
-			
+
 		} catch (Exception exc) {
 			exc.printStackTrace();
-			
+
 		}
 		return corso;
-		
-	}
-	}
 
+	}
 }
