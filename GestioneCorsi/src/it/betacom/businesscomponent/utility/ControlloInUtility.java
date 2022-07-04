c31278922ad364963e36b9bc47723079ec9a2adf
Betacom <Betacom@DESKTOP-30EVD2S.homenet.telecomitalia.it>
2022-07-04 11:10:32 +0200

    ControlloInUtility completo con tests -GC

--- GestioneCorsi/src/it/betacom/businesscomponent/utility/ControlloInUtility.java
+++ GestioneCorsi/src/it/betacom/businesscomponent/utility/ControlloInUtility.java
@@ -1,39 +1,50 @@
 package it.betacom.businesscomponent.utility;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import it.betacom.businesscomponent.facade.AdminFacade;
+import it.betacom.businesscomponent.model.Docente;
+
 public class ControlloInUtility {
 
 	public static String checkNomeCorsista(String in) {
-
-		if (StringUtils.isBlank(in))
-			return "Il campo del nome del corsista non deve essere nullo";
-		Pattern pattern = Pattern.compile("^[a-zA-Z]{2,30}$");
-		Matcher matcher = pattern.matcher(in);
-
-		if (matcher.find()) {
-			return null;
-		} else {
-			return "Il campo del nome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)";
-		}
-
+		return checkNome(in, "nome", "corsista");
 	}
 
 	public static String checkCognomeCorsista(String in) {
+		return checkNome(in, "cognome", "corsista");
+	}
+
+	public static String checkNomeCorso(String in) {
+		return checkNome(in, "nome", "corso");
+	}
+
+	public static String checkDataInizioCorso(String in) {
+		return checkData(in, "data inizio", "corso");
+	}
+
+	public static String checkDataFineCorso(String in) {
+		return checkData(in, "data fine", "corso");
+	}
+
+	public static String checkCommento(String in) {
+
 		if (StringUtils.isBlank(in))
-			return "Il campo del cognome del corsista non deve essere nullo";
-		Pattern pattern = Pattern.compile("^[a-zA-Z]{2,30}$");
+			return "Il campo del commento non deve essere nullo";
+
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9 .,?!()]{0,200}$");
 		Matcher matcher = pattern.matcher(in);
 
 		if (matcher.find()) {
 			return null;
 		} else {
-			return "Il campo del cognome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)";
+			return "Il campo del commento deve essere di massimo 200 caratteri";
 		}
-
 	}
 
 	public static String checkPrecedentiFormativiCorsista(String in) {
@@ -48,6 +59,54 @@ public class ControlloInUtility {
 			return "Il campo dei precedenti formativi del corsista deve essere SI o NO";
 		}
 
+	}
+
+	public static String checkAulaCorso(String in) {
+
+		if (StringUtils.isBlank(in))
+			return "Il campo aula di corso non deve essere nullo";
+
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9]{2,30}$");
+		Matcher matcher = pattern.matcher(in);
+
+		if (matcher.find()) {
+			return null;
+		} else {
+			return "Il campo aula di corso deve contenere da 2 a 30 lettere (a-z, A-Z, 0-9)";
+		}
+	}
+
+	public static Docente checkDocente(String cod) throws ClassNotFoundException, IOException, SQLException {
+		return AdminFacade.getInstance().findDocByCod(cod);
+	}
+
+	private static String checkNome(String in, String campo, String tabella) {
+
+		if (StringUtils.isBlank(in))
+			return "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		Pattern pattern = Pattern.compile("^[a-zA-Z]{2,30}$");
+		Matcher matcher = pattern.matcher(in);
+
+		if (matcher.find()) {
+			return null;
+		} else {
+			return "Il campo " + campo + " di " + tabella + " deve contenere da 2 a 30 lettere (a-z, A-Z)";
+		}
+	}
+
+	private static String checkData(String in, String campo, String tabella) {
+		if (StringUtils.isBlank(in))
+			return "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+
+		Pattern pattern = Pattern.compile(
+				"^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+		Matcher matcher = pattern.matcher(in);
+
+		if (matcher.find()) {
+			return null;
+		} else {
+			return "Il campo " + campo + " di " + tabella + " deve essere valida e in formato DD/MM/YYYY";
+		}
 	}
 
 }
--- GestioneCorsi/src/test/it/betacom/businesscomponent/utility/ControlloInUtilityTest.java
+++ GestioneCorsi/src/test/it/betacom/businesscomponent/utility/ControlloInUtilityTest.java
@@ -1,7 +1,12 @@
 package test.it.betacom.businesscomponent.utility;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,33 +16,34 @@ class ControlloInUtilityTest {
 
 	@Test
 	void testNomeCorsista() {
-		assertEquals(ControlloInUtility.checkNomeCorsista(null),
-				"Il campo del nome del corsista non deve essere nullo");
-		assertEquals(ControlloInUtility.checkNomeCorsista(""), "Il campo del nome del corsista non deve essere nullo");
-		assertEquals(ControlloInUtility.checkNomeCorsista("Sas1"),
-				"Il campo del nome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)");
-		assertEquals(ControlloInUtility.checkNomeCorsista("S"),
-				"Il campo del nome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)");
-		assertEquals(ControlloInUtility.checkNomeCorsista("Simoneeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
-				"Il campo del nome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)");
+		String campo = "nome";
+		String tabella = "corsista";
+		String nullo = "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		String regex = "Il campo " + campo + " di " + tabella + " deve contenere da 2 a 30 lettere (a-z, A-Z)";
+
+		assertEquals(ControlloInUtility.checkNomeCorsista(null), nullo);
+		assertEquals(ControlloInUtility.checkNomeCorsista(""), nullo);
+		assertEquals(ControlloInUtility.checkNomeCorsista("Sas1"), regex);
+		assertEquals(ControlloInUtility.checkNomeCorsista("S"), regex);
+		assertEquals(ControlloInUtility.checkNomeCorsista("Simoneeeeeeeeeeeeeeeeeeeeeeeeeeeeee"), regex);
 		assertNull(ControlloInUtility.checkNomeCorsista("Simone"));
 	}
 
 	@Test
 	void testCognomeCorsista() {
-		assertEquals(ControlloInUtility.checkCognomeCorsista(null),
-				"Il campo del cognome del corsista non deve essere nullo");
-		assertEquals(ControlloInUtility.checkCognomeCorsista(""),
-				"Il campo del cognome del corsista non deve essere nullo");
-		assertEquals(ControlloInUtility.checkCognomeCorsista("Sas1"),
-				"Il campo del cognome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)");
-		assertEquals(ControlloInUtility.checkCognomeCorsista("S"),
-				"Il campo del cognome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)");
-		assertEquals(ControlloInUtility.checkCognomeCorsista("Simoneeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
-				"Il campo del cognome del corsista deve contenere da 2 a 30 lettere (a-z, A-Z)");
+		String campo = "cognome";
+		String tabella = "corsista";
+		String nullo = "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		String regex = "Il campo " + campo + " di " + tabella + " deve contenere da 2 a 30 lettere (a-z, A-Z)";
+
+		assertEquals(ControlloInUtility.checkCognomeCorsista(null), nullo);
+		assertEquals(ControlloInUtility.checkCognomeCorsista(""), nullo);
+		assertEquals(ControlloInUtility.checkCognomeCorsista("Sas1"), regex);
+		assertEquals(ControlloInUtility.checkCognomeCorsista("S"), regex);
+		assertEquals(ControlloInUtility.checkCognomeCorsista("Simoneeeeeeeeeeeeeeeeeeeeeeeeeeeeee"), regex);
 		assertNull(ControlloInUtility.checkCognomeCorsista("Simone"));
 	}
-	
+
 	@Test
 	void testPrecedentiCorsista() {
 		assertEquals(ControlloInUtility.checkPrecedentiFormativiCorsista(null),
@@ -52,6 +58,101 @@ class ControlloInUtilityTest {
 				"Il campo dei precedenti formativi del corsista deve essere SI o NO");
 		assertNull(ControlloInUtility.checkPrecedentiFormativiCorsista("SI"));
 		assertNull(ControlloInUtility.checkPrecedentiFormativiCorsista("NO"));
+	}
+
+	@Test
+	void testNomeCorso() {
+		String campo = "nome";
+		String tabella = "corso";
+		String nullo = "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		String regex = "Il campo " + campo + " di " + tabella + " deve contenere da 2 a 30 lettere (a-z, A-Z)";
+
+		assertEquals(ControlloInUtility.checkNomeCorso(null), nullo);
+		assertEquals(ControlloInUtility.checkNomeCorso(""), nullo);
+		assertEquals(ControlloInUtility.checkNomeCorso("SQL1"), regex);
+		assertEquals(ControlloInUtility.checkNomeCorso("S"), regex);
+		assertEquals(ControlloInUtility.checkNomeCorso("SQLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL"), regex);
+		assertNull(ControlloInUtility.checkNomeCorso("SQL"));
+	}
+
+	@Test
+	void testNomeDataInizio() {
+		String campo = "data inizio";
+		String tabella = "corso";
+		String nullo = "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		String regex = "Il campo " + campo + " di " + tabella + " deve essere valida e in formato DD/MM/YYYY";
+
+		assertEquals(ControlloInUtility.checkDataInizioCorso(null), nullo);
+		assertEquals(ControlloInUtility.checkDataInizioCorso(""), nullo);
+		assertEquals(ControlloInUtility.checkDataInizioCorso("11/18/2000"), regex);
+		assertEquals(ControlloInUtility.checkDataInizioCorso("2000/18/11"), regex);
+		assertEquals(ControlloInUtility.checkDataInizioCorso("2000/11/18"), regex);
+		assertEquals(ControlloInUtility.checkDataInizioCorso("39/04/2000"), regex);
+		assertEquals(ControlloInUtility.checkDataInizioCorso("30/02/2000"), regex);
+		assertEquals(ControlloInUtility.checkDataInizioCorso("29/02/1999"), regex);
+		assertNull(ControlloInUtility.checkDataInizioCorso("18/04/2000"));
+		assertNull(ControlloInUtility.checkDataInizioCorso("29/02/2000"));
+	}
+
+	@Test
+	void testNomeDataFine() {
+		String campo = "data fine";
+		String tabella = "corso";
+		String nullo = "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		String regex = "Il campo " + campo + " di " + tabella + " deve essere valida e in formato DD/MM/YYYY";
+
+		assertEquals(ControlloInUtility.checkDataFineCorso(null), nullo);
+		assertEquals(ControlloInUtility.checkDataFineCorso(""), nullo);
+		assertEquals(ControlloInUtility.checkDataFineCorso("11/18/2000"), regex);
+		assertEquals(ControlloInUtility.checkDataFineCorso("2000/18/11"), regex);
+		assertEquals(ControlloInUtility.checkDataFineCorso("2000/11/18"), regex);
+		assertEquals(ControlloInUtility.checkDataFineCorso("39/04/2000"), regex);
+		assertEquals(ControlloInUtility.checkDataFineCorso("30/02/2000"), regex);
+		assertEquals(ControlloInUtility.checkDataFineCorso("29/02/1999"), regex);
+		assertNull(ControlloInUtility.checkDataFineCorso("18/04/2000"));
+		assertNull(ControlloInUtility.checkDataFineCorso("29/02/2000"));
+	}
+
+	@Test
+	void testCommento() {
+		String max = "";
+
+		for (int i = 0; i < 208; i++) {
+			max = max.concat("a");
+		}
+
+		String nullo = "Il campo del commento non deve essere nullo";
+		assertEquals(ControlloInUtility.checkCommento(null), nullo);
+		assertEquals(ControlloInUtility.checkCommento(""), nullo);
+
+		assertEquals(ControlloInUtility.checkCommento(max),
+				"Il campo del commento deve essere di massimo 200 caratteri");
+		assertNull(ControlloInUtility.checkCommento("Commento, Prova01."));
+	}
+
+	@Test
+	void testAulaCorso() {
+		String campo = "aula";
+		String tabella = "corso";
+		String nullo = "Il campo " + campo + " di " + tabella + " non deve essere nullo";
+		String regex = "Il campo " + campo + " di " + tabella + " deve contenere da 2 a 30 lettere (a-z, A-Z, 0-9)";
+
+		assertEquals(ControlloInUtility.checkAulaCorso(null), nullo);
+		assertEquals(ControlloInUtility.checkAulaCorso(""), nullo);
+		assertEquals(ControlloInUtility.checkAulaCorso("S"), regex);
+		assertEquals(ControlloInUtility.checkAulaCorso("Alfa0555555555555555555555555555555"), regex);
+		assertNull(ControlloInUtility.checkAulaCorso("Alfa05"));
+	}
+
+	@Test
+	void testDocente() {
+		try {
+			assertNotNull(ControlloInUtility.checkDocente("1"));
+			assertNull(ControlloInUtility.checkDocente("sas"));
+		} catch (ClassNotFoundException | IOException | SQLException e) {
+			e.printStackTrace();
+			fail("Eccezione durante la ricerca: " + e.getMessage());
+		}
 	}
 
 }
